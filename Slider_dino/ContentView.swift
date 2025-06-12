bdf304af49cfea4c5c90b0b4bf76ceb9e50588ff
OtterBell <stu9987765@gmail.com>
2025-06-12 14:43:41 +0800

    Initial Commit

---
+++ Slider_dino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Slider_dino
+//
+//  Created by 莊惠淇 on 2025/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Slider_dino/Slider_dinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slider_dinoApp.swift
+//  Slider_dino
+//
+//  Created by 莊惠淇 on 2025/6/12.
+//
+
+import SwiftUI
+
+@main
+struct Slider_dinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
